4098f4daa0fb6f40c54916680c0601766dc181ac
drmohamednassar <dr.mohamed.nassar.pt@gmail.com>
2026-09-01 22:08:08 +0300

    Add configuration for bot settings

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+  botName: "سورس محمد نصار",
+  ownerName: "محمد نصار",
+  ownerNumber: "4915511257385",
+  prefix: ".",
+  stickerPack: "سورس محمد نصار",
+  stickerAuthor: "محمد نصار"
+};
